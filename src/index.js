0c3bf1bf30a9b153f5d0da64c7e9e225bef0a191
Janice Teguh <janicejarusdy@gmail.com>
2023-01-19 20:28:07 +1100

    passed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+import { ThemeContext, ThemeProvider } from "./context/theme";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
